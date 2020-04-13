283a8e4d02d827780a3de95039101120c0f4bbb0
unknown <code.57x53@gmail.com>
2020-04-13 15:49:04 +0900

    #633 Div2 A,B 수정 완료. 너무 쉬운 문제들이었다... 영어공부 열심히 ㅠㅠ

--- #633 Div2/A.py
+++ #633 Div2/A.py
@@ -6,8 +6,5 @@ t = int(inp())
 while t:
     t-=1
     n=int(inp())
-    if n<=2:
-        print(n)
-    else:
-        print(2*(n-1)*(n))
+    print(n)
     
--- #633 Div2/B.py
+++ #633 Div2/B.py
@@ -8,39 +8,18 @@ while t:
     n = int(inp())
     tmp=[]
     arr=list(map(int,inp().split()))
-    arr= sorted(arr)
-    turn = 0
-    if arr[0]<0:                
-        if len(arr)%2==0:
-            right = len(arr)//2
-            left = right-1
-            while right<len(arr):
-                if turn==1:
-                    tmp.append(arr[left])
-                    tmp.append(arr[right])
-                    turn=0
-                else:                    
-                    tmp.append(arr[right])
-                    tmp.append(arr[left])
-                    turn=1
-                left-=1
-                right+=1
-        else:            
-            right = len(arr)//2
-            left = right-2
-            tmp.append(arr[right-1])
-            while right<len(arr):
-                if turn==0:
-                    tmp.append(arr[left])
-                    tmp.append(arr[right])
-                    turn=1
-                else:                    
-                    tmp.append(arr[right])
-                    tmp.append(arr[left])
-                    turn=0
-    else:
-        tmp=arr
-    for i in tmp:
-        print(i,end=' ')
+    arr= sorted(arr,reverse=True)
+                 
+    
+    right = n//2
+    left = right-1
+    while left >-1 or right<n:
+        if right<n:
+            print(arr[right],end=' ')
+            right+=1
+        if left>-1:
+            print(arr[left],end=' ')
+            left-=1
     print()
+    
     # print(a)  
