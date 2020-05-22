ca38ae313a3e5696950c4d1d8377385d731b42c6
CODe1995 <code.57x53@gmail.com>
2020-05-22 17:57:27 +0900

    643 B 정답
    오름차순 정렬후 cur을 기준으로 동일한 값이 있으면 카운트 증가,
    Editorial 참고하여 소스 작성함.

--- #643 Div2/B.py
+++ #643 Div2/B.py
@@ -1,2 +1,19 @@
 import sys
 input = sys.stdin.readline
+
+t= int(input())
+while t:
+    t-=1
+    e = int(input())
+    arr = list(map(int,input().strip().split()))
+    arr = sorted(arr)
+    ans = 0
+    cur = 0
+    i=0
+    while i<len(arr):
+        cur+=1
+        if cur==arr[i]:
+            ans+=1
+            cur=0
+        i+=1
+    print(ans)
