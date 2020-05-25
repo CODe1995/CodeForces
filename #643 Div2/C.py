930d9f1bbf332626b1370f051b2d11dcdafd1bc3
CODe1995 <code.57x53@gmail.com>
2020-05-25 23:10:02 +0900

    643 주석 수정

--- #643 Div2/C.py
+++ #643 Div2/C.py
@@ -8,7 +8,7 @@ for i in range(A, B+1):
     # print('i:',i,'\ni+B :',i+B,'\ni+C+1',i+C+1)
     a[i + B] += 1
     a[i + C + 1] -= 1
-#why twice added
+
 for i in range(1, N):
     a[i] += a[i - 1]
 for i in range(1, N):
