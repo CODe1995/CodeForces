450e14f0e75352ac05549422c8127102170b56ab
unknown <code.57x53@gmail.com>
2020-05-29 22:27:25 +0900

    ECR 88 Div2 B 통과
    "same row"!!!!!!!!!!!!!!!!!!!
    BFS, DFS로 접근했었는데,, same row를 보고 5분만에 해결했다...

--- #ECR 88 Div2/B.py
+++ #ECR 88 Div2/B.py
@@ -1,41 +1,35 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 t=int(input().strip())
-direction = [[0,1],[1,0],[0,-1],[-1,0]]
-nextq = deque()
-def bfs(field, valid):    
-    while nextq:
-        vx,vy=nextq.popleft()        
-        for dx,dy in direction:
-            tx = vx+dx
-            ty = vy+dy
-            if tx>=0 and tx<m and ty>=0 and ty<n and valid[ty][tx]==0:
-                if field[ty][tx] == '.':
-                    nextq.append([tx,ty])
-    return 0
 
 while t:
     ans=0
     t-=1
     n,m,x,y=map(int,input().split())
-    field=list('.'*n for _ in range(m))
-    valid=list([0]*n for _ in range(m))
+    field=list('.'*m for _ in range(n))
     
-    for i in range(m):
+    for i in range(n):
         field[i]=list(input().strip())
-    if x<y:
-        for i in field:
-            for j in i:
-                if j == '.':
-                    ans+=1
-        print(ans)
-    else:
+
+    if 2*x<=y:
         for i in range(n):
             for j in range(m):
-                bfs(field,valid,[j,i])
-
-            
+                if field[i][j]=='.':
+                    ans+=x
+    else:
+        for i in range(n):
+            j=0
+            while j+1<=m:
+                if field[i][j:j+2]==['.','.']:
+                    j+=2
+                    ans+=y
+                elif field[i][j]=='.':
+                    ans+=x
+                    j+=1
+                else:
+                    j+=1
+    print(ans)
+                
             
 
 
