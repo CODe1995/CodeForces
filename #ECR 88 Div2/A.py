5580eb29a33567b6a08dd159f76e2966809790dd
unknown <code.57x53@gmail.com>
2020-05-30 22:35:53 +0900

    ECR 88 A 풀이 완료
    노트를 활용해서 이해했음.

--- #ECR 88 Div2/A.py
+++ #ECR 88 Div2/A.py
@@ -1,11 +1,16 @@
 import sys
+import math
 input = sys.stdin.readline
 
 t=int(input().strip())
 while t:
     t-=1
     n,m,k=map(int,input().split())
-    d = n // k
-    a1 = min(m, d)
-    a2 = (m - a1 + k - 2) // (k - 1)
-    print(a1 - a2)
+    d = n // k  #한 사람당 가져가는 카드 수
+    x = min(m, d)   #첫 번째 사람이 가져가는 카드의 수
+    #if m>d: return d
+    #else: return m
+    #이렇게 써줘도 전혀 상관이 없다. 같은 방식인데 min으로하면 더 간단하다.
+
+    y = math.ceil((m-x)/(k-1)) #남은 사람중 가져가는 카드의 최대 수
+    print(x-y)
