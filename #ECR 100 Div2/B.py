2a664d9527ce7d70d4245900a63a9bdd2606cbc5
unknown <code.57x53@gmail.com>
2020-12-18 02:00:25 +0900

    풀이추가

--- #ECR 100 Div2/B.py
+++ #ECR 100 Div2/B.py
@@ -6,6 +6,12 @@ def ii():return int(input())
 def mii():return map(int,input().rstrip().split())
 def lmii():return list(map(int,input().rstrip().split()))
 ##########################################################
+'''
+풀이1)해당 수를 넘지 않는 수 중 가장 큰 2의 제곱수를 출력하면 된다.
+이유) 짝수는 항상 약수/배수의 관계임
+풀이2)홀수번째에 전부 1 넣고 나머지는 원래 수를 넣은 다음 값 계산
+조건 만족 안 하면 짝수번째에만 1 넣은거 출력하면 된다.
+'''
 t = ii()
 for _ in range(t):
     n = ii()
