99fcd2d3155012f44109881e16ecb0b7765ad2c9
unknown <code.57x53@gmail.com>
2020-12-18 01:51:06 +0900

    다시풀기

--- #ECR 100 Div2/A.py
+++ #ECR 100 Div2/A.py
@@ -5,14 +5,22 @@ def ii():return int(input())
 def mii():return map(int,input().rstrip().split())
 def lmii():return list(map(int,input().rstrip().split()))
 ##########################################################
-from math import ceil
+'''
+[문제]
+몬스터 세 마리를 죽인다.
+일반공격 6회 전체공격 1회를 반복한다.
+모든 몬스터는 반드시 전체공격때만 죽일 수 있다.
+
+[조건]
+1. 세 수의 합이 9의 배수
+2. 각 수가 최소한의 크기를 가져야 함
+'''
 t = ii()
 for _ in range(t):
     a,b,c = mii()
-    mx = max(a,b,c)
-    mx = mx//7
     s = a+b+c
-    if s%9==0 and a>=mx and b>=mx and c>=mx:
+    k = s//9
+    if s%9==0 and a>=k and b>=k and c>=k:
         print('YES')
     else:
         print('NO')
