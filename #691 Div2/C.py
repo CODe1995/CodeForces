941b43336c5119b5a4f1fdbcc92722b0cddc6c7b
unknown <code.57x53@gmail.com>
2020-12-20 15:09:11 +0900

    C 수정

--- #691 Div2/C.py
+++ #691 Div2/C.py
@@ -1,34 +1,21 @@
 ##########################################################
-import sys
+import sys,math
 from collections import deque
 input = sys.stdin.readline
-print = sys.stdout.write
+# print = sys.stdout.write
 def i():return input().rstrip()
 def ii():return int(input())
 def mii():return map(int,input().rstrip().split())
 def lmii():return list(map(int,input().rstrip().split()))
 ##########################################################
-def gcd(a,b):#if a>b
-    if a<b:a,b=b,a
-    while True:
-        r = a%b
-        if r==0: return b
-        a=b
-        b=r
-
-
 n,m = mii()
-A = lmii()
-B = lmii()
-prev = 1
-cur = 0
-for j in range(m):
-    abcnt=0
-    for i in range(n):
-        cur = A[i]+B[j]
-        abcnt+=1
-        if abcnt>1:
-            prev=gcd(prev,cur)
-            # print(i,j,prev,cur)
-        else:prev=cur
-    print(str(prev)+' ')
+a = lmii()
+b = lmii()
+var = 0
+for i in range(1,n):
+    print("gcd(%d, a[%d]%d-a[0]%d)"%(var,i,a[i],a[0]))
+    var = math.gcd(var,a[i]-a[0])
+for i in b:
+    print("gcd(%d, i%d+a[0]%d)"%(var,i,a[0]))
+    print(math.gcd(var,i+a[0]),end=" ")
+print()
