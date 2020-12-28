20554f8842c79b2a77393218b7f0683616642373
CODe1995 <code.57x53@gmail.com>
2020-12-28 21:13:16 +0900

    union Find

--- #692 Div2/C.py
+++ #692 Div2/C.py
@@ -8,26 +8,25 @@ def ii():return int(input())
 def mii():return map(int,input().rstrip().split())
 def lmii():return list(map(int,input().rstrip().split()))
 ##########################################################
-
 t = ii()
 for _ in range(t):
     graph = dict()
     n,m = mii()
-    # field = [[0]*n for _ in range(n)]
-    arr = list()
+    parent = [i for i in range(n+1)]
+    def getParent(x):
+        if parent[x]==x:return x
+        parent[x] = getParent(parent[x])
+        return parent[x]
+    def unionParent(x,y):
+        x=getParent(x)
+        y=getParent(y)
+        if x==y: return 1
+        parent[x]=y
+        return 0
+
+    res = 0
     for i in range(m):
         x,y = mii()
-        if x in graph:
-            graph[x].append([x,y])
-        else:
-            graph[x]=[[x,y]]
-        if y in graph:
-            graph[y].append([x,y])
-        else:
-            graph[y]=[[x,y]]
-    
-    for nodes in graph:
-        for i,node in enumerate(graph[nodes]):
-            if len(graph[node[0]])==1:#해당 구역에 혼자라면
-                graph[nodes][i]=
-        
+        if x==y:continue
+        res+=1+unionParent(x,y)
+    print(res)
